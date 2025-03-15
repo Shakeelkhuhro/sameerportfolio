ce54a9d3c2979a7e1c7972d679f9a5ba86d275cf
Shakeelkhuhro <shakeelahmedkhuhro5454@gmail.com>
2025-03-15 22:06:32 +0500

    publishing settings

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sameerporfolio/',  // <-- Update this with your repo name
+  base: '/sameerportfolio/',  // <-- Update this with your repo name
 });
