26091177923df02f61df1e2fd0f0c0317df0fd93
Shakeelkhuhro <shakeelahmedkhuhro5454@gmail.com>
2025-03-16 00:18:35 +0500

    final changes | nav-brand text change to SAMEER

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
   return (
     <nav className={`navbar navbar-expand-lg ${darkMode ? "dark-mode" : ""}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">MyPortfolio</Link>
+        <Link className="navbar-brand logo" to="/">Sameer</Link>
         <div className="d-lg-none">
           <DarkModeToggle />
         </div>
